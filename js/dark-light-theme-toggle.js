395a64a9a54865d237d44b31439cf8440fc67fdf
SudhinRaj <58643406+rjsudhin@users.noreply.github.com>
2025-05-04 10:24:40 +0530

    dark light them switching button is working

---
+++ js/dark-light-theme-toggle.js
@@ -0,0 +1,27 @@
+// dark light theme configure
+
+const rootElm = document.documentElement
+rootElm.classList.add('light')
+const darkLightToggle = document.querySelector('.dark-light-toggle')
+
+darkLightToggle.addEventListener('mousedown', () => {
+  changeTheme()
+})
+
+
+
+function changeTheme() {
+  let buttonText = ''
+  if (rootElm.className === 'light') {
+    rootElm.classList.remove('light')
+    rootElm.classList.add('dark')
+    buttonText = 'light'
+  } else {
+    rootElm.classList.remove('dark')
+    rootElm.classList.add('light')
+    buttonText = 'dark'
+  }
+
+  // change the text of button in with it
+  darkLightToggle.textContent = buttonText
+}

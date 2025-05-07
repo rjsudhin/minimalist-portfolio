d8b63a49906bf0659221e8282052ca158ab532e3
SudhinRaj <58643406+rjsudhin@users.noreply.github.com>
2025-05-07 08:35:31 +0530

    nav-bar aligned when it responsive

---
+++ js/hamburger.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('#hamburger')
+const navBar = document.querySelector('#nav-bar')
+
+hamburger.addEventListener('click', () => {
+  navBar.classList.toggle('active')
+})

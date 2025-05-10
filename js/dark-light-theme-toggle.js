c2039ed620828b0daf3dbc83e19fc99602f43717
SudhinRaj <58643406+rjsudhin@users.noreply.github.com>
2025-05-10 09:03:53 +0530

    now theme changing icons working perfetly

--- js/dark-light-theme-toggle.js
+++ js/dark-light-theme-toggle.js
@@ -13,12 +13,14 @@ function changeTheme() {
   if (rootElm.className === 'light') {
     rootElm.classList.remove('light')
     rootElm.classList.add('dark')
-    icon.classList.remove('fa-moon')
-    icon.classList.add('fa-sun')
+    icon.classList.remove('fa-sun')
+    icon.classList.add('fa-moon')
+    console.log('changed to dark theme')
   } else {
     rootElm.classList.remove('dark')
     rootElm.classList.add('light')
-    icon.classList.remove('fa-sun')
-    icon.classList.add('fa-moon')
+    console.log('changed to light theme')
+    icon.classList.remove('fa-moon')
+    icon.classList.add('fa-sun')
   }
 }
